21f86e29f9ecc68a7e70fb003d523dd9ae46a122
arifraj199 <karif9514@gmail.com>
2022-10-21 12:08:26 +0600

    homepage banner input added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div style={{
-            backgroundImage:`url("https://i.ibb.co/7rZV84V/bannerbackground.png")`,
-            backgroundSize:"cover",
-            backgroundPosition:'center',
-            height:'90vh',
-
-        }}>
-            
+  return (
+    <div
+      style={{
+        backgroundImage: `url("https://i.ibb.co/7rZV84V/bannerbackground.png")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "550px",
+      }}
+    >
+      <div className="pt-48">
+        <h1 className="text-3xl font-semibold">
+          Best Food Waiting For Your Belly
+        </h1>
+        <div className="items-center flex justify-center mt-5">
+          <input
+            type="text"
+            placeholder="Search Food items ..."
+            class="input w-full input-sm max-w-xs rounded-r-none rounded-l-full"
+          />
+          <button className="btn capitalize btn-sm rounded-l-none rounded-r-full bg-red-600 border-none hover:bg-red-700">Search</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
